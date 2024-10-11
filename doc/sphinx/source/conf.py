9b1ffe2fff0898cae6d72ae0f58d26e9c86aa420
Julien <Julienmiq@github.com>
2024-10-11 16:05:00 +0200

    First commit of the documentation

---
+++ doc/sphinx/source/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DmMtJj_info_indus_tutorial'
+copyright = '2024, Juanola Julien - Doulin Mathieu - Maetz Thibaut'
+author = 'Juanola Julien - Doulin Mathieu - Metz Thibaut'
+release = '11/10/2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.graphviz",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+    "sphinx_tabs.tabs",
+    "sphinx_rtd_theme",
+    "myst_parser",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for copybutton extension ----------------------------------------
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+
+html_static_path = ['_static']
+
+html_css_files = [
+    "css/custom.css",
+]
